25d89c6349729b5d540472d0dd0dd192ba004498
Robyn <Robyn@DESKTOP-GVS0L7V>
2016-10-05 13:38:47 +0100

    hello
    Robyn

--- CompanyNameHere/CompanyNameHere/Program.cs
+++ CompanyNameHere/CompanyNameHere/Program.cs
@@ -17,6 +17,7 @@ namespace CompanyNameHere
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // hello !
         }
     }
 }
